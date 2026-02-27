a9e9d54381589cc358bbcbd31dd6f65fa3d68bca
bmdigitalke <bmdigitalke@gmail.com>
2026-02-27 19:12:55 +0300

    Create Trading Dashboard Page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+// Trading Dashboard Page
+
+import React from 'react';
+
+const TradingDashboard = () => {
+    return (
+        <div>
+            <h1>Trading Dashboard</h1>
+            {/* Dashboard components will go here */}
+        </div>
+    );
+};
+
+export default TradingDashboard;
