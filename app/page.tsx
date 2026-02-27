d2346de86cb5ee197a8cb754d61d013a72118fd4
bmdigitalke <bmdigitalke@gmail.com>
2026-02-27 19:16:05 +0300

    Create app/page.tsx as the home page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Forex Trading Platform</h1>
+            <p>Your gateway to trading currencies!</p>
+        </div>
+    );
+};
+
+export default HomePage;
